f6123f75ff6585b17c04e34e3b9b903b5298ba62
Qwinton Knocklein <u21669849@tuks.co.za>
2025-10-31 21:02:27 +0200

    Updated packages, created models using db, started with Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using u21669849_HW3.Models;
 
 namespace u21669849_HW3.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        // GET: Home/Index
+        public async Task<ActionResult> Index()
         {
+            // Load Staff with related Store
+            ViewBag.Staffs = await db.staffs
+                .Include(s => s.stores)
+                .ToListAsync();
+
+            // Load Customers
+            ViewBag.Customers = await db.customers
+                .ToListAsync();
+
+            // Load Products with Brand and Category
+            ViewBag.Products = await db.products
+                .Include(p => p.brands)
+                .Include(p => p.categories)
+                .ToListAsync();
+
+            // Load Brands for filter dropdown
+            ViewBag.Brands = await db.brands.ToListAsync();
+
+            // Load Categories for filter dropdown
+            ViewBag.Categories = await db.categories.ToListAsync();
+
             return View();
         }
 
-        public ActionResult About()
+        protected override void Dispose(bool disposing)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
